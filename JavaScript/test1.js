3aaf7a2883dcc813b0921b811a7c21df1add17d0
Harish Krishnamoorthy <86472804+HarishKrish7878@users.noreply.github.com>
2021-08-08 00:34:31 -0700

    Project

---
+++ JavaScript/test1.js
@@ -0,0 +1,20 @@
+var mark = 90;
+if(mark>90 && mark<100){
+     console.log("Grade is A" );
+}
+else if(mark>80 && mark<90){
+    console.log("Grade is B");
+}
+else if(mark>70 && mark<80){
+    console.log("Grade is C");
+}
+else if(mark>60 && mark<70){
+    console.log("Grade is D");
+}
+else if(mark>45 && mark<60){
+    console.log("Grade is E");
+}
+else if(mark < 45){
+    console.log("mark is Fail");
+}
+ 
